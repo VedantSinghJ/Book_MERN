2cabda1df984605722334028bbac51c8a427c903
VedantSinghJ <vedantsingh.jadon10@gmail.com>
2025-01-17 20:42:06 +0530

    deployment changes

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 // })
 // );
 app.use(cors({
-    origin: ['https://bookcollection-three.vercel.app/', 'http://localhost:3000'],
+    origin: ['https://bookcollection-r947.onrender.com', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
 }));
