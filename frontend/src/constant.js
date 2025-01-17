b8437fb6148cbb0dea09b48ca2c127d817a2deb5
VedantSinghJ <vedantsingh.jadon10@gmail.com>
2025-01-17 15:56:13 +0530

    changes

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = process.env.NODE_ENV === 'production' 
-    ? 'https://bookcollection-backend.onrender.com'
-    : 'http://localhost:5555';
-
+// const API_BASE_URL = process.env.NODE_ENV === 'production' 
+//     ? 'https://bookcollection-backend.onrender.com'
+//     : 'http://localhost:5555';
+const API_BASE_URL = 'https://bookcollection-backend.onrender.com';
 export default API_BASE_URL;
